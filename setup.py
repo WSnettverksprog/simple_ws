e0a4c935395f49ac9fe0eba23ad4106ad98fb7d1
Ole Kristian Aune <okaune@outlook.com>
2017-05-03 15:49:43 +0200

    Testing description from readme

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
+
+try:
+   import pypandoc
+   description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   description = 'Simple websocket implementation in python using asyncio'
+    
 setup(
   name = 'simple_ws',
   packages = ['simple_ws'], 
   version = '0.2',
-  description = 'Simple websocket implementation in python using asyncio',
+  description = description,
   author = 'Ole Kristian Aune, Even Dalen, Audun Wigum Arbo',
   author_email = 'even.dalen@live.no',
   url = 'https://github.com/WSnettverksprog/python-WS', 
