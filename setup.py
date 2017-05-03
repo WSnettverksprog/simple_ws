311d58d3dcb3a1455e2c720c59335c514063124b
Ole Kristian Aune <okaune@outlook.com>
2017-05-03 15:57:25 +0200

    Fixed URL

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
   description = description,
   author = 'Ole Kristian Aune, Even Dalen, Audun Wigum Arbo',
   author_email = 'even.dalen@live.no',
-  url = 'https://github.com/WSnettverksprog/python-WS', 
-  download_url = 'https://github.com/WSnettverksprog/python-WS/archive/0.1.tar.gz', 
+  url = 'https://github.com/WSnettverksprog/simple_ws', 
+  download_url = 'https://github.com/WSnettverksprog/simple_ws/archive/0.1.tar.gz', 
   keywords = ['websocket', 'ws', 'asyncio', 'simple'], 
   classifiers = [],
 )
