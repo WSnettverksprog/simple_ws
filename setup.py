75f67e681e0bf652f07d5809bb2497700e976f92
Even Dalen <even.dalen@live.no>
2017-05-09 18:14:17 +0200

    Added support for splitting up TCP-packets

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from distutils.core import setup
 try:
    import pypandoc
    description = pypandoc.convert('README.md', 'rst')
-   description = long_description.replace("\r","")
+   description = description.replace("\r","")
 except (IOError, ImportError):
+
    description = 'Simple websocket implementation in python using asyncio'
-    
+
 setup(
   name = 'simple_ws',
   packages = ['simple_ws'], 
-  version = '0.2.1',
+  version = '0.2.2',
+  description='Simple websocket implementation in python using asyncio',
   long_description = description,
   author = 'Ole Kristian Aune, Even Dalen, Audun Wigum Arbo',
   author_email = 'even.dalen@live.no',
