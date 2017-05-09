890adc72838850dc9dd68a50a666eec571563875
olekau <olekau@student.hist.no>
2017-05-09 19:47:32 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -2,19 +2,21 @@ from distutils.core import setup
 
 try:
    import pypandoc
-   description = pypandoc.convert('README.md', 'rst')
-   description = description.replace("\r","")
+   long_description = pypandoc.convert('README.md', 'rst')
+   long_description = long_description.replace("\r","")
 except (IOError, ImportError):
-    description='Simple websocket implementation in python using asyncio',
+    long_description='',
 
 setup(
   name = 'simple_ws',
-  packages = ['simple_ws'], 
-  version = '0.2.2',
-  long_description = description,
+  packages = ['simple_ws'],
+  version = '0.3.0',
+  description = 'Simple websocket implementation in python using asyncio',
+  license = "MIT",
+  long_description = long_description,
   author = 'Ole Kristian Aune, Even Dalen, Audun Wigum Arbo',
   author_email = 'even.dalen@live.no',
-  url = 'https://github.com/WSnettverksprog/simple_ws', 
-  download_url = 'https://github.com/WSnettverksprog/simple_ws/archive/0.1.tar.gz', 
-  keywords = ['websocket', 'ws', 'asyncio', 'simple'], 
+  url = 'https://github.com/WSnettverksprog/simple_ws',
+  download_url = 'https://github.com/WSnettverksprog/simple_ws/archive/0.1.tar.gz',
+  keywords = ['websocket', 'ws', 'asyncio', 'simple'],
 )
